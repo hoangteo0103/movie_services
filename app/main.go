20387141f61fe31f3433d146f1c47c1866c9c514
Bui Nguyen Hoang <51512556+hoangteo0103@users.noreply.github.com>
2024-02-27 19:43:57 +0700

    Init project

---
+++ app/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	defaultTimeout = 30
+	defaultAddress = ":4000"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	//prepare database
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASS")
+	dbName := os.Getenv("DATABASE_NAME")
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module movie_services
+
+go 1.22.0
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
