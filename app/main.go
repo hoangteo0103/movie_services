8bff78aa1cab2eeaa2260619c093c730da0fdd5c
Bui Nguyen Hoang <51512556+hoangteo0103@users.noreply.github.com>
2024-02-27 20:23:48 +0700

    feat: Init migaration

--- app/main.go
+++ app/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -26,5 +28,21 @@ func main() {
 	dbUser := os.Getenv("DATABASE_USER")
 	dbPass := os.Getenv("DATABASE_PASS")
 	dbName := os.Getenv("DATABASE_NAME")
+
+	connStr := "user=" + dbUser + " dbname=" + dbName + " sslmode=disable password=" + dbPass + " host=" + dbHost + " port=" + dbPort
+
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	// Test the connection to the database
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Successfully Connected")
+	}
 
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module movie_services
 go 1.22.0
 
 require (
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
