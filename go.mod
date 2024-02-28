1581d8fd337ef95b31cc81780604bd0b642083fb
hoangteo0103 <buinguyenhoang0103@gmail.com>
2024-02-28 11:56:08 +0700

    feat: Init grpc server

--- go.mod
+++ go.mod
@@ -9,6 +9,11 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"log"
 	"net"
 	"os"
+	"store"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
 )
 
 func init() {
@@ -41,9 +43,22 @@ func main() {
 		log.Println("Successfully Connected")
 	}
 
+	querier := store.New(db)
+
+	// [Set up tcp]
+	s := grpc.NewServer()
 	lis, err := net.Listen("tcp", ":"+os.Getenv("GRPC_PORT"))
 	if err != nil {
 		log.Fatalln("Failed to listen", err)
 	}
+
+	// [grpc Server]
+	// Launch grpc Server
+	go func() {
+		log.Println("Serving gRPC on " + os.Getenv("SERVER_HOST") + ":" + os.Getenv("GRPC_PORT"))
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve GRPC server %v", err)
+		}
+	}()
 
 }
